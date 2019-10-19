0aa602b0ec8c1b011e3cf2c776f76fb8731303d0
hiraetho <fuyu@servyou.com.cn>
2019-10-19 10:35:13 +0800

    123

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  { path: 'dtl', loadChildren: './dtl/dtl.module#DtlPageModule' }
 ];
 @NgModule({
   imports: [
---
+++ src/app/dtl/dtl.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dtl',
+  templateUrl: './dtl.page.html',
+  styleUrls: ['./dtl.page.scss'],
+})
+export class DtlPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
